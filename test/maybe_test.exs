cfceace376ea0e5b406891b3e54498e21ffb8b29
Michel Rijnders <g.a.c.rijnders@gmail.com>
2013-10-20 22:20:32 +0200

    Tweak test messages

--- test/maybe_test.exs
+++ test/maybe_test.exs
@@ -3,25 +3,25 @@ defmodule MaybeTest do
   import Monad
   import Monad.Maybe
 
-  test "Maybe monad left identity" do
+  test "Monad.Maybe left identity" do
     f = fn (x) -> x * x end
     a = 2
     assert bind(return(a), f) == f.(a)
   end
 
-  test "Maybe monad right identity" do
+  test "Monad.Maybe right identity" do
     m = return 42
     assert bind(m, &return/1) == m
   end
 
-  test "Maybe monad associativity" do
+  test "Monad.Maybe associativity" do
     f = fn (x) -> x * x end
     g = fn (x) -> x - 1 end
     m = return 2
     assert bind(return(bind(m, f)), g) == bind(m, &bind(return(f.(&1)), g))
   end
 
-  test "Maybe monad bind success" do
+  test "Monad.Maybe successful bind" do
     assert (m_do Monad.Maybe do
               x <- just 2
               y <- just 4
@@ -29,7 +29,7 @@ defmodule MaybeTest do
             end) == {:just, 8}
   end
 
-  test "Maybe monad bind fail" do
+  test "Monad.Maybe failing bind" do
     assert (m_do Monad.Maybe do
               x <- just 2
               y <- fail "Yes, we can"
