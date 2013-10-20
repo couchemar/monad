2726831df7ca45323bdd259c7afca3a2bb16cd9a
Michel Rijnders <g.a.c.rijnders@gmail.com>
2013-10-20 21:14:32 +0200

    Test monad laws for Maybe monad

--- test/monad_test.exs
+++ test/monad_test.exs
@@ -3,6 +3,24 @@ defmodule MonadTest do
   import Monad
   import Monad.Maybe
 
+  test "Maybe monad left identity" do
+    f = fn (x) -> x * x end
+    a = 2
+    assert bind(return(a), f) == f.(a)
+  end
+
+  test "Maybe monad right identity" do
+    m = return 42
+    assert bind(m, &return(&1)) == m
+  end
+
+  test "Maybe monad associativity" do
+    f = fn (x) -> x * x end
+    g = fn (x) -> x - 1 end
+    m = return 2
+    assert bind(return(bind(m, f)), g) == bind(m, &bind(return(f.(&1)), g))
+  end
+
   test "Maybe monad bind success" do
     assert (m_do Monad.Maybe do
               x <- return 2
