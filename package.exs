5857f38f9845a13d49c59d5bd40208a3e5368656
Michel Rijnders <g.a.c.rijnders@gmail.com>
2013-10-21 15:33:23 +0200

    Bump version number (properly this time)

--- package.exs
+++ package.exs
@@ -1,4 +1,4 @@
 Expm.Package.new(name: "monad", description: "Haskell style monads and do-syntax",
-                 version: "0.1", keywords: ["Elixir","library","monads","Haskell"],
+                 version: "0.2.1", keywords: ["Elixir","library","monads","Haskell"],
                  maintainers: [[name: "Michel Rijnders", email: "g.a.c.rijnders@gmail.com"]],
-                 repositories: [[github: "rmies/monad", tag: "0.2"]])
+                 repositories: [[github: "rmies/monad", tag: "0.2.1"]])
