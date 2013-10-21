1a779e479dff49c9d670019f2198c9f0a87a5ecf
Michel Rijnders <g.a.c.rijnders@gmail.com>
2013-10-21 13:39:02 +0200

    Simplify macro expansion
    There is no need to expand return statements: `return` can just be
    imported into the local scope. Thanks to Peter Minten (pminten)
    for the hint.

--- lib/monad.ex
+++ lib/monad.ex
@@ -1,7 +1,4 @@
 defmodule Monad do
-  def expand(monad, [{:return, line, x}]) do
-    {{:., line, [monad, :return]}, line, x}
-  end
   def expand(_, [action]) do
     action
   end
@@ -15,17 +12,9 @@ defmodule Monad do
     end
   end
   def expand(monad, [action | actions]) do
-    e = case action do
-          {:return, _, _} ->
-            expand(monad, [action])
-          _ ->
-            action
-        end
     quote do
-      f = fn (_) ->
-        unquote(expand(monad, actions))
-      end
-      unquote(monad).bind(unquote(e), f)
+      f = fn (_) -> unquote(expand(monad, actions)) end
+      unquote(monad).bind(unquote(action), f)
     end
   end
 
