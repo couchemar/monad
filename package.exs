d2911becb430781b588cf2e534c700de1e7068c8
Michel Rijnders <g.a.c.rijnders@gmail.com>
2013-10-21 10:03:58 +0200

    Add package definition

---
+++ package.exs
@@ -0,0 +1,4 @@
+Expm.Package.new(name: "monad", description: "Haskell style monads and do-syntax",
+                 version: "0.1", keywords: ["Elixir","library","monads","Haskell"],
+                 maintainers: [[name: "Michel Rijnders", email: "g.a.c.rijnders@gmail.com"]],
+                 repositories: [[github: "rmies/monad", tag: "0.1"]])
