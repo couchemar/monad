d2383ab14de77916c8aa92837052d878eba14c3b
Michel Rijnders <g.a.c.rijnders@gmail.com>
2013-10-22 10:52:07 +0200

    Tweak layout

--- lib/monad.ex
+++ lib/monad.ex
@@ -12,26 +12,27 @@ defmodule Monad do
     end
   end
 
-  defp expand(mod, [{ :let, _, let_exprs } | exprs]) do
+  defp expand(mod, [{:let, _, let_exprs} | exprs]) do
     if length(let_exprs) == 1 and is_list(hd(let_exprs)) do
       case Keyword.fetch(hd(let_exprs), :do) do
-        :error     -> let_exprs ++ expand(mod, exprs)
+        :error ->
+          let_exprs ++ expand(mod, exprs)
         { :ok, e } ->
-          [ e | expand(mod, exprs) ]
+          [e | expand(mod, exprs)]
       end
     else
       let_exprs ++ expand(mod, exprs)
     end
   end
-  defp expand(mod, [{ :<-, _, [lhs, rhs] } | exprs]) do
-    # x <- m  ==>  bind(b, fn x -> ... end)
+  defp expand(mod, [{:<-, _, [lhs, rhs]} | exprs]) do
+    # x <- m ==> bind(b, fn x -> ... end)
     expand_bind(mod, lhs, rhs, exprs)
   end
-  defp expand(_, [ expr ]) do
+  defp expand(_, [expr]) do
     [expr]
   end
-  defp expand(mod, [ expr | exprs ]) do
-    # m       ==>  bind(b, fn _ -> ... end)
+  defp expand(mod, [expr | exprs]) do
+    # m ==> bind(b, fn _ -> ... end)
     expand_bind(mod, quote(do: _), expr, exprs)
   end
   defp expand(_, []) do
@@ -40,7 +41,10 @@ defmodule Monad do
 
   defp expand_bind(mod, lhs, rhs, exprs) do
     [quote do
-      unquote(mod).bind(unquote(rhs), fn unquote(lhs) -> unquote_splicing(expand(mod, exprs)) end)
+      unquote(mod).bind(unquote(rhs),
+                        fn unquote(lhs) ->
+                             unquote_splicing(expand(mod, exprs))
+                        end)
     end]
   end
 
