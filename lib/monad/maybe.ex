681f88a7522b8ffcea60db251927da995c785549
Michel Rijnders <g.a.c.rijnders@gmail.com>
2013-10-29 13:13:14 +0100

    Rename Maybe monad type from `maybe` to `maybe_m`

--- lib/monad/maybe.ex
+++ lib/monad/maybe.ex
@@ -31,11 +31,11 @@ defmodule Monad.Maybe do
       :nothing
   """
 
-  @type maybe :: {:just, any} | :nothing
+  @type maybe_m :: {:just, any} | :nothing
 
   ## Monad implementations
 
-  @spec bind(maybe, (any -> any)) :: maybe
+  @spec bind(maybe_m, (any -> any)) :: maybe_m
   def bind(m, f)
   def bind({:just, x}, f), do: f.(x)
   def bind(:nothing, _), do: :nothing
@@ -43,7 +43,7 @@ defmodule Monad.Maybe do
   @doc """
   Injects `x` into a Maybe monad.
   """
-  @spec return(any) :: maybe
+  @spec return(any) :: maybe_m
   def return(x), do: {:just, x}
 
   ## Auxiliary functions
@@ -51,14 +51,14 @@ defmodule Monad.Maybe do
   @doc """
   Signal failure.
   """
-  @spec fail(any) :: maybe
+  @spec fail(any) :: maybe_m
   def fail(_), do: :nothing
 
   @doc """
   Call function `f` with `x` if `m` is `{:just, x}`, otherwise call function `f`
   with default value `d`.
   """
-  @spec maybe(any, (any -> any), maybe) :: any
+  @spec maybe(any, (any -> any), maybe_m) :: any
   def maybe(d, f, m)
   def maybe(_, f, {:just, x}), do: f.(x)
   def maybe(d, f, :nothing), do: f.(d)
@@ -66,21 +66,21 @@ defmodule Monad.Maybe do
   @doc """
   Returns true if given `{:just, x}` and false if given `:nothing`.
   """
-  @spec is_just(maybe) :: boolean
+  @spec is_just(maybe_m) :: boolean
   def is_just({:just, _}), do: true
   def is_just(:nothing), do: false
 
   @doc """
   Returns true if given `:nothing` value and false if given `{:just, x}`.
   """
-  @spec is_nothing(maybe) :: boolean
+  @spec is_nothing(maybe_m) :: boolean
   def is_nothing(:nothing), do: true
   def is_nothing({:just, _}), do: false
 
   @doc """
   Extracts value `x` out of `{:just, x}` or raises an error if given `:nothing`.
   """
-  @spec from_just(maybe) :: any
+  @spec from_just(maybe_m) :: any
   def from_just(m)
   def from_just({:just, x}), do: x
   def from_just(:nothing), do: raise "Monad.Maybe.from_just: :nothing"
@@ -89,7 +89,7 @@ defmodule Monad.Maybe do
   Extracts value `x` out of `{:just, x}` or returns default `d` if given
   `:nothing`.
   """
-  @spec from_maybe(any, maybe) :: any
+  @spec from_maybe(any, maybe_m) :: any
   def from_maybe(d, m)
   def from_maybe(_, {:just, x}), do: x
   def from_maybe(d, :nothing), do: d
@@ -109,7 +109,7 @@ defmodule Monad.Maybe do
       [42]
 
   """
-  @spec maybe_to_list(maybe) :: [any]
+  @spec maybe_to_list(maybe_m) :: [any]
   def maybe_to_list(m)
   def maybe_to_list({:just, x}), do: [x]
   def maybe_to_list(:nothing), do: []
@@ -129,7 +129,7 @@ defmodule Monad.Maybe do
       {:just, 1}
 
   """
-  @spec list_to_maybe([any]) :: maybe
+  @spec list_to_maybe([any]) :: maybe_m
   def list_to_maybe(l)
   def list_to_maybe([x | _]), do: {:just, x}
   def list_to_maybe([]), do: :nothing
@@ -143,7 +143,7 @@ defmodule Monad.Maybe do
       [1, 2, 3]
 
   """
-  @spec cat_maybes([maybe]) :: [any]
+  @spec cat_maybes([maybe_m]) :: [any]
   def cat_maybes(l) do
     lc x inlist l, is_just(x), do: from_just x
   end
@@ -152,7 +152,7 @@ defmodule Monad.Maybe do
   Map function `f` over the list `l` and throw out elements for which `f`
   returns `:nothing`.
   """
-  @spec map_maybes((any -> maybe), [any]) :: [any]
+  @spec map_maybes((any -> maybe_m), [any]) :: [any]
   def map_maybes(f, l) do
     lc x inlist l, is_just(f.(x)), do: from_just f.(x)
   end
