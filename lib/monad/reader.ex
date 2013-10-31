548ec6bddfcb4a6cb258cb23231b2c7047e26f89
Michel Rijnders <g.a.c.rijnders@gmail.com>
2013-10-31 22:37:07 +0100

    Tweak `Monad.Reader` documentation

--- lib/monad/reader.ex
+++ lib/monad/reader.ex
@@ -4,14 +4,14 @@ defmodule Monad.Reader do
   @moduledoc """
   The Reader monad.
 
-  Allows passing read-only values "under the hood".
+  Monad that encapsulates a read-only value/shared environment.
 
   ## Examples
 
       iex> require Monad.Reader, as: Reader
       iex> import Reader
       iex> r = Reader.m do
-      ...>       a <- return 2
+      ...>       let a = 2
       ...>       b <- ask
       ...>       return a + b
       ...>     end
@@ -19,34 +19,37 @@ defmodule Monad.Reader do
       12
   """
 
-  # A reader is just a function that receives the "under the hood" value.
-  @opaque reader_m :: ((any) -> any)
+  # A reader is just a function that receives the read-only value.
+  @opaque reader_m :: (any -> any)
 
   ## Monad implementations
 
-  @spec bind(reader_m, ((any) -> reader_m)) :: reader_m
+  @spec bind(reader_m, (any -> reader_m)) :: reader_m
   def bind(r, f), do: fn x -> f.(r.(x)).(x) end
 
+  @doc """
+  Inject `x` into a Reader monad.
+  """
   @spec return(any) :: reader_m
   def return(x), do: fn _ -> x end
 
   ## Other functions
 
   @doc """
-  Run the reader by supplying the given value to it.
+  Run Reader monad `r` by supplying it with value `x`.
   """
   @spec run(any, reader_m) :: any
   def run(x, r), do: r.(x)
 
   @doc """
-  Ask for the reader's value.
+  Ask for the Reader monad's value.
   """
   @spec ask() :: reader_m
   def ask(), do: fn x -> x end
 
   @doc """
-  Locally set a different value for the reader.
+  Set a different value locally for the Reader monad.
   """
-  @spec local(reader_m, ((any) -> any)) :: reader_m
+  @spec local(reader_m, (any -> any)) :: reader_m
   def local(r, f), do: fn x -> r.(f.(x)) end
 end
