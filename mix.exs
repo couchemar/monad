2216290cb1aca6d910de4197797ae3a2eb6aca36
Michel Rijnders <g.a.c.rijnders@gmail.com>
2013-11-04 14:13:51 +0100

    Bump version number

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Monad.Mixfile do
 
   def project do
     [app: :monad,
-     version: "0.4",
+     version: "1.0",
      name: "monad",
      source_url: "https://github.com/rmies/monad",
      elixir: "~> 0.11.0",
